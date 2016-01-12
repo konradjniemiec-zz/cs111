e39a13b1449d233a5c19ad41f0a0d822a8c5affa
avikbmohan <avikbmohan@gmail.com>
2016-01-12 13:33:12 -0800

    Update main.c
    Added code

--- main.c
+++ main.c
@@ -2,29 +2,41 @@
 #include <stdio.h>
 #include <getopt.h>
 
-int verboseFlag = 0;
+//////
+#include <unistd.h> //for access in openFile
 
 int main (int argc, char **argv){
   int op;
   struct option long_opts[]
   {
     {"verbose", no_argument, NULL, "v"},
-      {"command", required_argument, NULL, 'c'},
-	{"rdonly",required_argument,NULL, 'r'},
-	  {"wronly",required_argument,NULL,'w' }
-    //more options go here
+    {"command", required_argument, NULL, 'c'},//'c' -> O_WRONLY?
+    {"rdonly",required_argument,NULL, 'r'}, //'r' -> O_RDONLY?
+    {"wronly",required_argument,NULL,'w' }
   };
   int option_index = 0;
+  int isVerbose = 0;
 
-  int c = getopt_long(argc, argv,long_opts,&option_index);
+//malloc the space for everyhting here??
 
-  if (c== -1)
+//
+  //while(true){ ???
+//
+
+int x = getopt_long(argc, argv,long_opts,&option_index);
+
+  if (x== -1)
     break;
+    //capture error?
 
-  switch (c) {
+  switch (x) {
     case 'v': {
+    	isVerbose = 1
+    	break;
     }
-    case 'r': {
+    case 'r': 
+    case 'w': { //'r' -> O_RDONLY?
+      openFile(x)
     }
     case 'c': {
 
@@ -32,8 +44,39 @@ int main (int argc, char **argv){
     case '?':
       break;
   }
+}
+
   
-  
+void OpenFile(int flagg){
+	if (isVerbose == 1)
+	//this is a complicated print
+	//printf("%s-Only\n", ((c=='r') ? "READ" : "WRITE")
+	
+	//attempt to find it
+	//use a switch on c to create  a string R_OK, W_OK, etc to use in access, calling it s
+	  //seemslike a lot of ppl juts use F_OK though
+	//Also Iknow you use optarg here but what the hell cant find anything on it
+	  // like some secret keyword no one talks about
+	  
+	/*
+	  		// Check to make sure that all of the requested descriptors exist
+	for (int i = 0; i < 3; i++) {
+		if (command_args[i][0] - '0' > n_files) {
+			fprintf(stderr, "error: bad file descriptor\n");
+			return NULL;
+		}
+	}
+	*/
+	  
+	  
+	if (access(opt_arg, s) != -1){
+		//good
+	}
+	else{
+		fprintf("ERROR OPENING FILE")
+		exit();
+	}
+}
   /*
     create structure for getopt_long
     while (we have an option left) {
