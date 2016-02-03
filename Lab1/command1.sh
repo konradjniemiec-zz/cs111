8152698bb709927c7cd20dbb3ceef59981e65417
Konrad Julian Niemiec <konrad@lnxsrv02.seas.ucla.edu>
2016-02-02 21:48:12 -0800

    added simpsh for command1.sh

--- Lab1/command1.sh
+++ Lab1/command1.sh
@@ -6,6 +6,7 @@ time cat aasdf | tr ' ' '\n' | tr '[a-z]' '[A-Z]' | sort > out
 
 printf "Simpsh Results:\n"
 
+time ./simpsh --rdwr aasdf --pipe --pipe --pipe --trunc --wronly out --command 0 2 2 cat aasdf --command 1 4 4 tr ' ' '\n' --command 3 6 6 tr '[a-z]' '[A-Z]' --command 5 7 7 sort --wait
 
 printf "Exceline Results:\n"
 
