1af044a25ff34e4bffe14ccfe8a4e7d1fbb3af1a
Konrad Julian Niemiec <konrad@lnxsrv02.seas.ucla.edu>
2016-02-02 22:10:37 -0800

    added simpsh code to command2 and command3

--- Lab1/command2.sh
+++ Lab1/command2.sh
@@ -18,14 +18,18 @@ sort asdf > afsd | \
 shuf afsd > asdf | \
 sort asdf > afsd | \
 shuf afsd > asdf | \
-cat asdf
+cat afsd
 
 
 printf "SimpSh Results:\n"
 
+time ./simpsh --rdonly a --rdwr asdf --rdwr afsd --pipe --pipe --pipe --pipe --pipe --pipe --pipe --pipe --creat --wronly temp --command 0 4 4 cat a --command 3 6 6 tr ' ' '\n' --command 5 8 8 sort --command 7 10 10 shuf afsd --command 9 12 12 sort asdf --command 11 14 14 shuf afsd --command 13 16 16 sort asdf --command 15 18 18 shuf afsd --command 17 19 19 cat --wait
+
+# 0 1 2 3-4 5-6 7-8 9-10 11-12 13-14 15-16 17-18
 
 printf "Exceline Results:\n"
 
-rm asdf
-rm afsd
+
 rm a
+rm afsd
+rm asdf
--- Lab1/command3.sh
+++ Lab1/command3.sh
@@ -7,6 +7,9 @@ time cat b | tr ' ' '\n' | sort | grep "abc"
 
 printf "SimpSh Results:\n"
 
+time ./simpsh --rdonly b --pipe --pipe --pipe --creat --wronly temp --command 0 2 2 cat b --command 1 4 4 tr ' ' '\n' --command 3 6 6 sort --command 5 7 7 grep "abc" --wait
+
 printf "Exceline Results:\n"
 
 rm b
+rm temp
