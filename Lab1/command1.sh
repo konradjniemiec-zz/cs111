7a35826da56a79c87fb4b23dda287ea37db625e3
avikbmohan <avikbmohan@gmail.com>
2016-02-02 21:34:19 -0800

    Update command1.sh

--- Lab1/command1.sh
+++ Lab1/command1.sh
@@ -1,2 +1,12 @@
 #!/usr/local/cs/bin/bash
+head -c 500 /dev/urandom > aasdf
 
+printf "Bash Results:\n"
+time cat aasdf | tr ' ' '\n' | tr '[a-z]' '[A-Z]' | sort > out
+
+printf "Simpsh Results:\n"
+
+
+printf "Exceline Results:\n"
+
+rm aasdf
